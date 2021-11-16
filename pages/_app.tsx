c7463d5230538b5da154ac442ef56bc55b40c4c8
dkibru <kibdemta@gmail.com>
2021-11-16 14:29:20 +0300

    removing socket references

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,30 +2,14 @@
 import 'tailwindcss/tailwind.css'
 
 import type { AppProps } from 'next/app'
-
-// import { io } from "socket.io-client";
+ 
 
 
 function MyApp({ Component, pageProps }: AppProps) {
-  
-  // // const socket = io("http://localhost:3001");
-  // const socket = io('http://localhost:3001' , {transports: ['websocket']});
-
-  // socket.on("connect", () => {
-  //   console.log(socket.id); // x8WIv7-mJelg7on_ALbx
-  // });
-  
-  // socket.on("disconnect", () => {
-  //   console.log(socket.id); // undefined
-  // });
-
-  // let s = {
-  //   socket : socket
-  // }
+   
 
   let props = {
-      ...pageProps,
-      // ...s
+      ...pageProps
   };
 
 
